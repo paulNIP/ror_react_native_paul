8be3655c0de0d921c03b90a5cb3af5d3147aaee4
Paul Otim <paulalex.otim@outlook.com>
2023-11-22 18:02:15 +0400

    Updated the Audio Player

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 import App from './App';
 import {name as appName} from './app.json';
+import { playbackService } from './src/service/trackPlayerServices';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
+
+
